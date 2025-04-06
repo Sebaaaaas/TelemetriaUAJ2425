5e32c81b8192a2d44dfb9773b66e1045e2f774a4
Sebaaaaas <sebsan01@ucm.es>
2025-04-06 13:38:44 +0200

    Proeycto Telemetria en C#

---
+++ TelemetriaDOC/TelemetriaDOC/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TelemetriaDOC
+{
+    public class Class1
+    {
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
