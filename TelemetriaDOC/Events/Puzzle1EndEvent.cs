02ca6d5af2eb962b9ec209369e39c73363739d35
Sebaaaaas <sebsan01@ucm.es>
2025-04-19 12:03:32 +0200

    Puzzle 1&2 Start&End Event

---
+++ TelemetriaDOC/Events/Puzzle1EndEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetriaDOC
+{
+    public class Puzzle1EndEvent : Event
+    {
+        private Guid gameID;
+        public Puzzle1EndEvent(float timestamp, Guid _gameID) : base(timestamp)
+        {
+            gameID = _gameID;
+        }
+        public override string getName() { return "Puzzle1EndEvent"; }
+        public override string serializeToJSON()
+        {
+            string s = "  {\n";
+            s += base.serializeToJSON();
+            s += ",\n";
+            s += "    \"GameID\" : " + gameID;
+            s += "\n  }";
+            return s;
+        }
+    }
+}
---
+++ TelemetriaDOC/Events/Puzzle2EndEvent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetriaDOC
+{
+    public class Puzzle2EndEvent : Event
+    {
+        private Guid gameID;
+        public Puzzle2EndEvent(float timestamp, Guid _gameID) : base(timestamp)
+        {
+            gameID = _gameID;
+        }
+        public override string getName() { return "Puzzle2EndEvent"; }
+        public override string serializeToJSON()
+        {
+            string s = "  {\n";
+            s += base.serializeToJSON();
+            s += ",\n";
+            s += "    \"GameID\" : " + gameID;
+            s += "\n  }";
+            return s;
+        }
+    }
+}
