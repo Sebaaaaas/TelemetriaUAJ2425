fad92d186639ced0fc89b5806188361b770d3376
manuelpr <manuelpr@ucm.es>
2025-04-19 13:04:05 +0200

    PlayerAtackEvent

---
+++ TelemetriaDOC/Events/PlayerAttackEvent.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetriaDOC
+{
+    public class PlayerAttackEvent : Event
+    {
+        public PlayerAttackEvent(float timestamp) : base(timestamp)
+        {
+
+        }
+        public override string getName() { return "PlayerAtack"; }
+        public override string serializeToJSON()
+        {
+            string s = "  {\n";
+            s += base.serializeToJSON();
+            s += ",\n";
+            s += "    \"PlayerAtack\" : " + true;
+            s += "\n  }";
+            return s;
+        }
+    }
+}
