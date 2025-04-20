41ec4932c9859b3f37924ea460c9d5ac45c3299d
GonzaloFdezMoreno <48910540+GonzaloFdezMoreno@users.noreply.github.com>
2025-04-20 18:30:54 +0200

    evento GameState para diferentes partidas

---
+++ TelemetriaDOC/Events/GameState.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetriaDOC
+{
+    public class GameStateEvent:Event
+    {
+        public enum EventType { GameStart, GameEnd };
+        EventType _type;
+        public GameStateEvent(float timestamp, EventType type) : base(timestamp)
+        {
+            _type = type;
+        }
+
+        public override string getName() { return "GameStateEvent"; }
+        public override string serializeToJSON()
+        {
+            string s = "  {\n";
+            s += base.serializeToJSON();
+            s += ",\n";
+            s += "    \"Type\" : " + _type.ToString();
+            s += "\n  }";
+            return s;
+        }
+
+
+
+
+    }
+}
