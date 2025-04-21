634334aa555dbd3db82f16cb776defa5fe457c14
oscarpb99 <oscarped@ucm.es>
2025-04-21 15:57:49 +0200

    Eventos de Reseteo del puzle 2 y de cuando lo supera

---
+++ TelemetriaDOC/Events/Puzzle2ResetEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TelemetriaDOC.Events
+{
+    public class Puzzle2ResetEvent : Event
+    {
+        public Puzzle2ResetEvent(float timestamp) : base(timestamp)
+        {
+            name = "Puzzle2ResetEvent";
+        }
+
+        public override string serializeToJSON()
+        {
+            string s = "{";
+            s += base.serializeToJSON();
+            s += "}";
+            return s;
+        }
+    }
+}
---
+++ TelemetriaDOC/Events/Puzzle2SuccessEvent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Linq;
+
+namespace TelemetriaDOC.Events
+{
+    public class Puzzle2SuccessEvent:Event
+    {
+        public Puzzle2SuccessEvent(float timestamp) : base(timestamp)
+        {
+            name = "Puzzle2SuccessEvent";
+        }
+
+        public override string serializeToJSON()
+        {
+            string s = "{";
+            s += base.serializeToJSON();
+            s += "}";
+            return s;
+        }
+    }
+}
